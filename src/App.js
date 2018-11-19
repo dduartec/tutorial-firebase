6bdd8d38b04e96316c7cb6faacce06c93d0721ea
Diego Duarte <dduartec@unal.edu.co>
2018-11-18 21:29:20 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -2,24 +2,64 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import firebase from 'firebase';
+
 class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      user: null,
+    }
+
+    this.handleLogOut = this.handleLogOut.bind(this);
+    this.renderizarBotones = this.renderizarBotones.bind(this);
+  }
+
+  componentWillMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({
+        user: user
+      })
+    })
+  }
+  handleLogOut() {
+    firebase.auth().signOut();
+  }
+
+  renderizarBotones() {
+    if (this.state.user) {
+      return (
+        <div>
+          <h2>Sesión Iniciada</h2>
+          <h3>{this.state.user.displayName}</h3>
+          <button onClick={this.handleLogOut()}></button>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <h2>Inicia Sesión</h2>
+          <button className="button g">Google</button><br></br>
+          <button className="button f">Facebook</button><br></br>
+          <button className="button t">Twitter</button><br></br>
+        </div>
+      )
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>
+          Tutorial Firebase
+        </h1>
+        <div>
+          {
+            this.renderizarBotones()
+          }
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import firebase from 'firebase';
+
+firebase.initializeApp({
+    apiKey: "AIzaSyARQmafc0cPC7cyeubxs-gH5WpzMUoQmME",
+    authDomain: "tutorial-firebase-82a04.firebaseapp.com"
+})
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
