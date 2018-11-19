f556cfe19c2e70f495e38d16e233e1224a87efe3
Sergio <stpremiun@gmail.com>
2018-11-19 12:14:27 -0500

    Agrego autenticacion con facebook

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends Component {
     }
 
     this.handleLogOut = this.handleLogOut.bind(this);
+    //this.LoginWithFacebook = this.LoginWithFacebook.bind(this);
     this.renderizarBotones = this.renderizarBotones.bind(this);
   }
 
@@ -34,7 +35,7 @@ class App extends Component {
         <div>
           <h2>Sesión Iniciada</h2>
           <h3>{this.state.user.displayName}</h3>
-          <button onClick={this.handleLogOut()}></button>
+          <button onClick={() => this.handleLogOut()}>Cerrar Sesión</button>
         </div>
       )
     } else {
@@ -42,11 +43,32 @@ class App extends Component {
         <div>
           <h2>Inicia Sesión</h2>
           <button className="button g">Google</button><br></br>
-          <button className="button f">Facebook</button><br></br>
+          <button onClick={() => this.LoginWithFacebook()} className="button f">Facebook</button><br></br>
           <button className="button t">Twitter</button><br></br>
         </div>
       )
     }
+  }
+  
+  LoginWithFacebook(){
+	var provider = new firebase.auth.FacebookAuthProvider();
+	provider.addScope('email');
+	firebase.auth().signInWithPopup(provider).then(function(result) {
+		// This gives you a Facebook Access Token. You can use it to access the Facebook API.
+		var token = result.credential.accessToken;
+		// The signed-in user info.
+		var user = result.user;
+		// ...
+	}).catch(function(error) {
+		// Handle Errors here.
+		var errorCode = error.code;
+		var errorMessage = error.message;
+		// The email of the user's account used.
+		var email = error.email;
+		// The firebase.auth.AuthCredential type that was used.
+		var credential = error.credential;
+		// ...
+	});
   }
 
   render() {
