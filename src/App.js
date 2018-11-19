6932ef19caff0824a24c486021fd204df4e003eb
Brayan Garzon <begarzonf@unal.edu.co>
2018-11-19 12:36:00 -0500

    autenticacion con GitHub
    autenticacion con GitHub

--- src/App.js
+++ src/App.js
@@ -44,12 +44,32 @@ class App extends Component {
           <h2>Inicia Sesión</h2>
           <button className="button g">Google</button><br></br>
           <button onClick={() => this.LoginWithFacebook()} className="button f">Facebook</button><br></br>
-          <button className="button t">Twitter</button><br></br>
+          <button onClick={() => this.LoginWithGitHub()} className="button t">GitHub</button><br></br>
         </div>
       )
     }
   }
   
+  LoginWithGitHub(){
+    var provider = new firebase.auth.GithubAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      // ...
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+  }
+
   LoginWithFacebook(){
 	var provider = new firebase.auth.FacebookAuthProvider();
 	provider.addScope('email');
